d19ab4d43ef6ba1165071b4951c858e525fd7723
Georgiana Lingurariu <Georgiana.Lingurariu@endava.com>
2018-10-29 14:34:14 +0200

    First commit

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,7 @@
+// ActionTypes are defined in constants.
+
+// They are imported in Actions and Reducers.
+
+export const INCREMENT_NUM = "INCREMENT_NUM";
+export const DECREMENT_NUM = "DECREMENT_NUM";
+export const RESET = "RESET";

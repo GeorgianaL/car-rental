b961c7adcad24003938d7921ce76b8ba195f04f3
Georgiana Lingurariu <Georgiana.Lingurariu@endava.com>
2018-10-30 14:52:55 +0200

    Style landpage

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './style.scss';
+
 const Header = () => (
   <div className="header">
     <div className="header__logo">
--- src/components/landpage/Landpage.component.jsx
+++ src/components/landpage/Landpage.component.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 
+import './style.scss';
+
+const text = {
+  'title': 'BEST in City',
+  'subtitle': 'TRUSTED Services in Car Rental',
+  'steps': [
+    {
+      'title': 'Pick a car online',
+      'description': 'by browsing and selecting THE ONE',
+    },
+    {
+      'title': 'Get',
+      'description': 'from the nearest parking lot',
+    },
+    {
+      'title': 'Return',
+      'description': 'wherever you want',
+    }
+  ]
+};
+
 const Landpage = () => (
   <div className="landpage">
-    Landpage
+    <div className="title">
+      <p className="title__first">{text.title}</p>
+      <p className="title__second">{text.subtitle}</p>
+    </div>
+    <div className="steps">
+    {
+      text.steps.map((step, repeatIndex) => (
+        <div className="step">
+          <div className="step__index">{repeatIndex}</div>
+            <div className="step__description">
+              <p>{step.title}</p>
+              <p>{step.description}</p>
+          </div>
+        </div>
+      ))
+    }
+    </div>
   </div>
 );
 
